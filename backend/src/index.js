1e45a2a3548af77db6b7af5131fbc5572bbc6142
Augusto Nepomuceno <augustofnl@gmail.com>
2020-02-15 09:07:54 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 mongoose.connect(
-  "mongodb+srv://afnl:102030ab@cluster0-byxqw.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv:",
   {
     useNewUrlParser: true
   }
